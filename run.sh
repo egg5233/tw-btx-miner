47551d17c5696a3e1ad6363e39dcda5b9f1f2acb
david <luckydavid5235@gmail.com>
2026-07-05 07:59:16 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Launch tw-btx-miner with the bundled CUDA runtime on the library path.
-cd "$(dirname "$0")" || exit 1
-exec env LD_LIBRARY_PATH="$PWD:$LD_LIBRARY_PATH" ./btx-miner "$@"
